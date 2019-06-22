54d95b991ac88c5f511682e9a7a11a634a09249a
Manvel Saroyan <saroyanm@gmail.com>
2019-06-22 13:23:49 +0400

    Added cmints starter

---
+++ config.js
@@ -0,0 +1,22 @@
+const templateData =
+{
+  site: {
+    title: "CMintS Starter", // See "layouts/_head.ejs"
+    navigation: [
+      {
+        title: "CMintS",
+        link: "https://cmints.io/"
+      },
+      {
+        title: "Tutorial",
+        link: "https://cmints.io/quick-start"
+      },
+      {
+        title: "Github",
+        link: "https://github.com/cmints/single-lang-starter"
+      }
+    ]
+  }
+};
+
+module.exports = {templateData};
---
+++ pages/page2.md
@@ -0,0 +1,11 @@
+---
+title: Second page
+description: Get your webiste up and running online in no time using current starter
+header: CMintS starter
+---
+
+## Second page
+
+This is the second page of CMintS that is using Markdown.
+
+[Go to the Homepage](/)
